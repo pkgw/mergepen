75f51bf35ce521a222b71d557d380c89409fb522
Peter Williams <peter@newton.cx>
2025-08-12 22:43:26 -0400

    Parse a root folder doc ID nicely

--- src/repo.rs
+++ src/repo.rs
@@ -2,20 +2,63 @@
 //! Automerge/Samod document repository.
 
 use anyhow::Result;
-use samod::{ConnDirection, DocumentId, PeerId, Samod, storage::TokioFilesystemStorage};
-use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use samod::DocumentId;
+use serde::{
+    Deserialize, Deserializer, Serialize,
+    de::{Error as DeError, Visitor},
+};
+use std::{
+    fmt::{self, Display},
+    marker::PhantomData,
+    path::PathBuf,
+    str::FromStr,
+};
 
 use crate::doc_repo::DocRepo;
 
+/// Copy-paste: https://users.rust-lang.org/t/serde-fromstr-on-a-field/99457/5
+///
+/// DocumentId can be deserialized from a string by serde, but it requires a
+/// written-out UUID, not the base58check preferred in automerge stuff.
+fn deserialize_from_str<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: FromStr,
+    T::Err: Display,
+    D: Deserializer<'de>,
+{
+    struct Helper<S>(PhantomData<S>);
+    impl<'de, S> Visitor<'de> for Helper<S>
+    where
+        S: FromStr,
+        <S as FromStr>::Err: Display,
+    {
+        type Value = S;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(formatter, "a string")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+        where
+            E: DeError,
+        {
+            value.parse::<Self::Value>().map_err(DeError::custom)
+        }
+    }
+
+    deserializer.deserialize_str(Helper(PhantomData))
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct RepoConfig {
+    #[serde(deserialize_with = "deserialize_from_str")]
     root_folder_id: DocumentId,
 }
 
 pub struct Repository {
     work_root: PathBuf,
     repo_root: PathBuf,
+    config: RepoConfig,
 }
 
 impl Repository {
@@ -27,14 +70,23 @@ impl Repository {
             p
         };
 
-        let mut docs_root = repo_root.clone();
-        docs_root.push("docs");
-        std::fs::create_dir_all(docs_root)?;
+        let config_path = {
+            let mut p = repo_root.clone();
+            p.push("config.toml");
+            p
+        };
+        let config_data = std::fs::read(config_path)?;
+        let config = toml::from_slice(&config_data)?;
 
-        Ok(Repository {
+        let repo = Repository {
             work_root,
             repo_root,
-        })
+            config,
+        };
+
+        std::fs::create_dir_all(repo.doc_repo_root())?;
+
+        Ok(repo)
     }
 
     pub fn doc_repo_root(&self) -> PathBuf {
